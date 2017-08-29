7caba635e2331a3d42b50c173ec0c07b25185e09
anmaikul <michaelan0925@gmail.com>
2017-08-29 14:31:15 -0400

    Update set_item_task.py

--- set_item_task.py
+++ set_item_task.py
@@ -2,8 +2,8 @@ import girder_client
 import girder_worker_utils
 from script import largest_counties
 
-API_KEY='8wqEZFR2aqwIZVLJbAPLK4zwq09NK3rV9ToyYD59'  # Set to your own API key
-ITEM_ID='59a1ad244f2ae50460333137'  # Set to your own item ID
+API_KEY='HuN3lMD5UIPiP2NKfZi81FEx8eB1u6cGsIxtfw98'  # Set to your own API key
+ITEM_ID='59a5b11d4f2ae5377cd5a1d6'  # Set to your own item ID
 
 client = girder_client.GirderClient(apiUrl='https://resonant-demo.kitware.com/api/v1')
 client.authenticate(apiKey=API_KEY)
